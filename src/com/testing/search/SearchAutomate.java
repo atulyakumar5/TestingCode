18554f2de697c197837e4b052af723403d8597e8
Atulya Kumar <atulyakumar5@gmail.com>
2021-11-07 00:53:41 +0530

    Sleep time Changed

---
+++ src/com/testing/search/SearchAutomate.java
@@ -0,0 +1,31 @@
+package com.testing.search;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SearchAutomate {
+
+	static WebDriver driver;
+	public static void main(String[] args) throws InterruptedException {
+		
+		driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		
+		driver.get("https://www.google.com/");
+		
+		WebElement search = driver.findElement(By.xpath("//input[contains(@type,'text')]"));
+		search.clear();
+		search.sendKeys("flipkart.com");
+		search.sendKeys(Keys.ENTER);
+		
+		Thread.sleep(1000);
+		
+		driver.close();
+	}
+}
